3d879f4b62231f849b6bf541d80538a22202174a
Ananya <90745373+anunicn47@users.noreply.github.com>
2023-02-25 17:40:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import env
 import logging
-__import__("os").system("pip install -U -r requirements.txt")
+
 from pyrogram import Client, idle
 from pyromod import listen  # type: ignore
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
