aaf2f5f690b942a9eb0eed016bf82c4712688d4a
Ananya <90745373+anunicn47@users.noreply.github.com>
2023-03-18 21:56:55 +0530

    Update users_sql.py

--- StringSessionBot/database/users_sql.py
+++ StringSessionBot/database/users_sql.py
@@ -1,13 +1,20 @@
+from env import DATABASE_URL
 from sqlalchemy import Column, BigInteger
-from StringSessionBot.database import BASE, SESSION
 
 
+if DATABASE_URL !="":
+    from StringSessionBot.database import BASE, SESSION
+else:
+    BASE = "..."
+    
 class Users(BASE):
     __tablename__ = "users"
     __table_args__ = {'extend_existing': True}
     user_id = Column(BigInteger, primary_key=True)
 
     def __init__(self, user_id, channels=None):
+        if DATABASE_URL == "":
+            return
         self.user_id = user_id
         self.channels = channels
 
@@ -15,11 +22,14 @@ class Users(BASE):
     #     return "<User {} {} {} ({})>".format(self.thumbnail, self.thumbnail_status, self.video_to, self.user_id)
 
 
-Users.__table__.create(checkfirst=True)
+if DATABASE_URL !="":
+
+    Users.__table__.create(checkfirst=True)
 
 
 async def num_users():
-    try:
-        return SESSION.query(Users).count()
-    finally:
-        SESSION.close()
+    if DATABASE_URL !="":
+        try:
+            return SESSION.query(Users).count()
+        finally:
+            SESSION.close()
